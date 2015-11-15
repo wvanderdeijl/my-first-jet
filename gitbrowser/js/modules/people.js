dbf6f0ab826f484df9d5b99abbdbb56ee3173a72
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2015-11-15 16:07:51 +0100

    show list of (possibly filtered) github users
    included oj.Collection to make the REST call to github api and auto-submit of filter field to viewModel and use it in the query

--- gitbrowser/js/modules/people.js
+++ gitbrowser/js/modules/people.js
@@ -1,19 +1,34 @@
 /**
  * People module
  */
-define(['knockout', 'ojs/ojcore', 'jquery', 'ojs/ojinputtext', 'ojs/ojbutton'], function (ko, oj, $) {
+define(['knockout', 'ojs/ojcore', 'ojs/ojrouter', 'ojs/ojmodel', 'ojs/ojinputtext', 'ojs/ojbutton', 'ojs/ojtable'], function (ko, oj) {
     /**
      * The view model for the People module
      */
     function PeopleViewModel() {
+        // ----- public fields and methods -----
         var peopleViewModel = this;
         peopleViewModel.searchName = ko.observable("");
-        peopleViewModel.searchClick = searchClick;
-        
-        // ----- implementation details -----
-        function searchClick() {
-            alert('clicked');
-        }
+        peopleViewModel.users = ko.observable();
+        peopleViewModel.usersDatasource = ko.observable(); // datasource for ojTable
+
+        // ----- private implementation details -----
+        var baseUrl = "https://api.github.com/";
+        var UsersCollection = oj.Collection.extend({
+            url: "https://api.github.com/users?per_page=100",
+            customURL: function () {
+                var search = peopleViewModel.searchName();
+                var url = baseUrl + (search ? "search/users?q=" + encodeURIComponent(search) : "users?per_page=100");
+                return url;
+            }
+        });
+        peopleViewModel.users(new UsersCollection());
+        peopleViewModel.usersDatasource(new oj.CollectionTableDataSource(peopleViewModel.users()));
+        peopleViewModel.searchName.subscribe(function () {
+            // reset collection when search field changes to force requery
+            // without even using a button or form submit
+            peopleViewModel.users().reset();
+        });
     }
     return PeopleViewModel;
 });
