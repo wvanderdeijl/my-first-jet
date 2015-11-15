6d71509e8c535dfbe9380e5603b8e579a1553870
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2015-11-15 11:34:52 +0100

    initial PeopleViewModel to show search box and button

--- gitbrowser/js/modules/people.js
+++ gitbrowser/js/modules/people.js
@@ -1,5 +1,19 @@
-/* 
- * Your viewModel code goes here
+/**
+ * People module
  */
-
-
+define(['knockout', 'ojs/ojcore', 'jquery', 'ojs/ojinputtext', 'ojs/ojbutton'], function (ko, oj, $) {
+    /**
+     * The view model for the People module
+     */
+    function PeopleViewModel() {
+        var peopleViewModel = this;
+        peopleViewModel.searchName = ko.observable("");
+        peopleViewModel.searchClick = searchClick;
+        
+        // ----- implementation details -----
+        function searchClick() {
+            alert('clicked');
+        }
+    }
+    return PeopleViewModel;
+});
