fa0c129282ac0e62b4294a6f693e6d06b02e3f43
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2015-11-27 23:14:57 +0100

    better refreshing of the data collection
    also added calculation of maximum score

--- gitbrowser/js/modules/people.js
+++ gitbrowser/js/modules/people.js
@@ -1,35 +1,41 @@
 /**
  * People module
  */
-define(['knockout', 'ojs/ojcore', 'ojs/ojrouter', 'ojs/ojmodel', 'ojs/ojinputtext', 'ojs/ojbutton', 'ojs/ojtable', 'ojs/ojlistview'], function (ko, oj) {
+define(['knockout', 'ojs/ojcore', 'jquery', 'ojs/ojrouter', 'ojs/ojknockout-model', 'ojs/ojmodel', 
+        'ojs/ojinputtext', 'ojs/ojbutton', 'ojs/ojtable', 'ojs/ojlistview'], 
+function (ko, oj, $) {
     /**
      * The view model for the People module
      */
-    function PeopleViewModel() {
+    function PeopleViewModel(params) {
         // ----- public fields and methods -----
         var peopleViewModel = this;
         peopleViewModel.searchName = ko.observable("");
         peopleViewModel.users = ko.observable();
         peopleViewModel.usersDatasource = ko.observable(); // datasource for ojTable
+        peopleViewModel.maxScore = ko.observable();
 
         // ----- private implementation details -----
         var UsersCollection = oj.Collection.extend({customURL: _buildUrl});
-        peopleViewModel.users(new UsersCollection());
         peopleViewModel.searchName.subscribe(function () {
-            _fetch();
+            peopleViewModel.users().refresh().then(function(x){
+                console.log(x);
+                // unfortunately an ojListView doesn't refresh itself when
+                // the underlying collection refresh, so we have to do it
+                // ojTable does do it automatically.
+                $('#ghusers').ojListView('refresh')
+                var row0 = peopleViewModel.users().at(0);
+                peopleViewModel.maxScore(row0 && row0.get('score'));
+            });
         });
-        _fetch();
-        
+        peopleViewModel.users(new UsersCollection());
+        peopleViewModel.usersDatasource(new oj.CollectionTableDataSource(peopleViewModel.users()));
         // ----- methods -----
         function _buildUrl() {
             var baseUrl = "https://api.github.com/";
             var search = peopleViewModel.searchName();
             var url = baseUrl + (search ? "search/users?q=" + encodeURIComponent(search) : "users?per_page=100");
             return url;
-        }
-        function _fetch() {
-            peopleViewModel.users().reset();
-            peopleViewModel.usersDatasource(new oj.CollectionTableDataSource(peopleViewModel.users()));
         }
     }
     return PeopleViewModel;
